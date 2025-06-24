64ef3dad4a9f91456034b3b533ff89f8cf9da01b
Soiidux <soiidux040@gmail.com>
2025-06-25 01:43:21 +0530

    new python file

---
+++ day1/bandname.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator")
+#taking name of city and pet as input
+city=input("What is the name of the city you grew up in? :")
+pet_name=input("What is your pet's name?: ")
+#printing the suggested band name on the screen
+band_name=city+" "+pet_name
+print("Your band name could be:",band_name)
